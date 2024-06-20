dde56735d30b6dead6bd29f2e40d15b438a4bbec
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-06-20 11:46:12 +0200

    Script de prueba revisado

--- prueba.sh
+++ prueba.sh
@@ -2,6 +2,8 @@
 
 REGISTRY="${1:-registro-docker.test}"
 
+docker login $REGISTRY
+
 for image in alpine:latest alpine:edge alpine:3.20 alpine:3.19 alpine:3.18 alpine:3.17; do
   docker pull $image
   docker tag $image $REGISTRY/$image
